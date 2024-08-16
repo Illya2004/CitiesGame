10089bd9beb6022ed1787356e4d20fbc36a6b189
Illya Oboronov <i.oboronov@gmail.com>
2024-08-16 22:44:36 +0300

    fix: Refactor cities.txt path

--- src/main/java/org/kolis1on/citiesgame/service/TxtReader.java
+++ src/main/java/org/kolis1on/citiesgame/service/TxtReader.java
@@ -1,20 +1,29 @@
 package org.kolis1on.citiesgame.service;
 
 import org.kolis1on.citiesgame.exception.SymbolIsNotLetterException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.ContextLoaderListener;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.FileSystem;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 @Component
 public class TxtReader {
+    private static final String FILE_PATH = Paths.get("src", "main", "resources", "cities.txt").
+            toFile().
+            getAbsolutePath();
 
-    private static final String FILE_PATH = "D:\\CitiesGame\\src\\main\\resources\\cities.txt";
+
     public List<String> getCitiesByLetter(char letter) {
         List<String> cities = new ArrayList<>();
 
