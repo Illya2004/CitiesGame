5e986c5aae63a9e2b5ad226bc7f1668abebd4989
Illya Oboronov <i.oboronov@gmail.com>
2024-08-17 09:42:31 +0300

    fix: Disable cors

--- src/main/java/org/kolis1on/citiesgame/CitiesGameApplication.java
+++ src/main/java/org/kolis1on/citiesgame/CitiesGameApplication.java
@@ -2,6 +2,12 @@ package org.kolis1on.citiesgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CitiesGameApplication {
---
+++ src/main/java/org/kolis1on/citiesgame/config/SpringConfig.java
@@ -0,0 +1,28 @@
+package org.kolis1on.citiesgame.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*"); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return new CorsFilter(source);
+    }
+
+}
