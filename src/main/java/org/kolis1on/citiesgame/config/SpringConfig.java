21517bd888cbe83e68064acf3d09b9b0dbdb8f43
Illya Oboronov <i.oboronov@gmail.com>
2024-08-17 10:51:16 +0300

    fix: Disable cors

--- src/main/java/org/kolis1on/citiesgame/config/SpringConfig.java
+++ src/main/java/org/kolis1on/citiesgame/config/SpringConfig.java
@@ -14,7 +14,7 @@ public class SpringConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5500"));
         configuration.setAllowedMethods(List.of("GET", "POST"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("*"); // Allow all headers
